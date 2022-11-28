5f0997d433e30ce5f82d2a92ac7683e42a315f20
kalyani <kalyanibagul@nimapinfotech.com>
2022-11-28 18:37:57 +0530

    simple crud task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [name , setName] = useState('');
+    const [email , setEmail] = useState('');
+    const [password , setPassword] = useState('');
+    const [confirmpassword , setconfirmpassword] = useState('')
+    const handleChange =(e)=>{
+      setName(e.target.value);
+    }
+    const handleEmailChange =(e)=>{
+      setEmail(e.target.value);
+    }
+    const handlePasswordChange =(e)=>{
+      setPassword(e.target.value);
+    }
+    const handleConfPasswordChange =(e)=>{
+      setconfirmpassword(e.target.value);
+    }
+    const handleSubmit=(e)=>{
+      if(password !==confirmpassword )
+      { alert("password Not Match");
+    }
+    else{
+      alert('A form was submitted with Name :"' + name  +'" and Email :"' + email + '"');
+      }
+      e.preventDefault();
+ 
+    }
+       return(
+        <div>
+            <form onSubmit={(e) => {handleSubmit(e)}}> 
+              <label>Name : </label>
+              <input type="text" name="name" required onChange={(e) => {handleChange(e)}}/>
+              <br/>
+              <label>Email :</label>
+              <input type="text" email="email" required onChange={(e) => {handleEmailChange(e)}} />
+              <br/>
+              <label>Password</label>
+              <input type="text" password="password" required onChange={(e) => {handlePasswordChange(e)}}/>
+              <br/>
+              <label>Confirm Password</label>
+              <input type="text" confirmpassword="confirmpassword" required onChange={(e) => {handleConfPasswordChange(e)}} />
+              <br/>
+              <input type="submit" value="Submit"/>
+            </form> 
+        </div>
+       )
+}    
 
 export default App;
